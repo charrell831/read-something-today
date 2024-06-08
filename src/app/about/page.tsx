261b6c2f796af8ec421fc4359e7a4e358b4e020d
charrell831 <cjh266@scarletmail.rutgers.edu>
2024-06-07 20:06:01 -0400

    made podcast page dynamic

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,12 +5,12 @@ export default function About() {
     return (
         <div className="overflow-x-hidden bg-black">
             <NavigationBar />
-            <h1 className="pt-6 mb-4 flex text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            <h1 className="px-20 pt-6 mb-4 flex text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
                 Welcome.
             </h1>
-            <p className="">
+            <p className="px-20 flex justify-center">
                 This website was created with the intention of educating those with a willingness to learn.
-                Education is an essential tool in the fight for liberation and this website is (hopefully) a tool
+                I believe that education is an essential tool in the fight for liberation and this website is (hopefully) a tool
                 that will assist people in doing so.
                 <br />
                 <br />
@@ -22,6 +22,10 @@ export default function About() {
                 <br />
                 My hope is that anyone who comes across this site walks away with new knowledge that aligns with 
                 progressive values.
+                <br />
+                <br />
+                <br />
+                Thank you.
             </p>
         </div>
     );
--- src/app/components/podcastDisplay.tsx
+++ src/app/components/podcastDisplay.tsx
@@ -1,12 +1,30 @@
+import useMediaQuery from "../useMedia"
+
 export default function PodcastDisplayComponent(podcastObject: any) {
-    return (   
-        <div className="justify-center flex">
+    let isPhone = useMediaQuery("(max-width: 576px)")
+    console.log(isPhone)
+    return (
+        <div>
+        {isPhone ? (
+            <div className="justify-center flex">
+            <iframe className="border-radius:12px" 
+                src={podcastObject.podcastObject.podLink}
+                width="75%" 
+                height="600"
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">     
+            </iframe>
+        </div>
+        ):(
+            
+            <div className="justify-center flex ">
             <iframe className="border-radius:12px " 
                 src={podcastObject.podcastObject.podLink}
                 width="50%" 
                 height="352" 
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
             </iframe>
+        </div>
+        )}   
         </div>
     )
 }
