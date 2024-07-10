2c0811a2ade0fa696ecb0265367c0ccd4d98fbf5
charrell831 <cjh266@scarletmail.rutgers.edu>
2024-07-10 13:24:39 -0400

    fix build fail hopefully

--- src/app/authorsCorner/rowOne/page.tsx
+++ src/app/authorsCorner/rowOne/page.tsx
@@ -22,7 +22,7 @@ async function statementSelected(statement: string) {
     });
 }
 
-export default function RowOne(statement: string[]) {
+export default function RowOne() {
     <>
         <Button 
             className="p-0 bg-transparent data-[hover=true]:bg-transparent text-white"  
