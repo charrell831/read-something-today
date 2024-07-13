9288f2baa7a86b0e00dcc359bbf4c31b43f7c4fc
charrell831 <cjh266@scarletmail.rutgers.edu>
2024-07-13 16:04:35 -0400

    curious if build will pass after file fixes

--- src/app/authorsCorner/rowOne/page.tsx
+++ src/app/authorsCorner/rowOne/page.tsx
@@ -6,6 +6,7 @@ import NavigationBar from "@/app/components/navBar";
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useEffect } from 'react';
+import { AuthorType } from '../page';
 
 let statement = ''
 let row: any[] = []
@@ -64,7 +65,7 @@ export default function RowOne() {
                         <Link 
                             className="px-6 py-5 w-full m-auto text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" 
                             //children being past in url is messed up
-                            href={{pathname: pathname + '/rowTwo', query: r}}
+                            href={{pathname: pathname + '/rowTwo', query: {r, imageSrc}}}
                             key={r.statement}
                         >
                             <div className='text-center'>
