1a6de5317a9c78323b8939fcdb25f6495dd12890
charrell831 <cjh266@scarletmail.rutgers.edu>
2024-07-22 20:22:58 -0400

    changing link for next config file

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
                 source: '/api/:path*',
                 destination: 
                 process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5000/api/:path*' 
-                    : 'production' ? 'https://resistance-learn-2ah16vtng-charrell831s-projects.vercel.app/api/:path*'
+                    : 'production' ? 'https://www.readsomethingtoday.com/api/:path*'
                     : '/api/',
             },
           //   {
